f683838548c326dbb8f12b5cfaa27505f7cdc023
IT21440236 <it21440236@my.sliit.lk>
2023-07-14 17:32:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import AppBar from "./components/Appbar";
+import Student from "./components/Student";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppBar />
+      <Student />
+    </div>
+  );
 }
 
 export default App;
